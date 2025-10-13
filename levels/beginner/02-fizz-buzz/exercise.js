eb5aa107964d8732f5ef10368bf0b520525b2af1
Tio-Mando <armandoalbb@gmail.com>
2025-10-13 18:03:49 -0400

    ejercicio

--- levels/beginner/02-fizz-buzz/exercise.js
+++ levels/beginner/02-fizz-buzz/exercise.js
@@ -12,11 +12,55 @@
  */
 
 function fizzBuzz(n) {
-    // TODO: Implementar la solución aquí
+    let valorActual = 1
+    let resultado = []
+    for(let i = 0; i < n ; i ++){
+      if(valorActual % 3 === 0 && valorActual % 5 === 0){
+            resultado.push('FizzBuzz')
+        } else if(valorActual % 3 === 0){
+            resultado.push('Fizz')            
+        } else if(valorActual % 5 === 0){
+            resultado.push('Buzz')
+            
+        } else {
+            resultado.push(valorActual.toString())
+            
+        }
+        valorActual = valorActual + 1
+    }
+    console.log(resultado)
+    return resultado
+
     
-    // Pista: Usa el operador módulo (%) para verificar múltiplos
     
-    throw new Error('Función no implementada');
+    // // TODO: Implementar la solución aquí
+    
+    // // Pista: Usa el operador módulo (%) para verificar múltiplos
+    
+    // throw new Error('Función no implementada');
 }
+let n = 5
+
+fizzBuzz(n)
+// let valorActual = 15
+// let valoS = valorActual.toString()
+// console.log(typeof valoS)
+// console.log(valoS)
+
+// console.log(valorActual % 3 === 0 && valorActual % 5 === 0)
+
+// let aa = []
+// aa.push('aaa')
+// console.log(aa)
+
+// if (3 % 3 === 0){
+//     console.log('es primo')
+// } else {
+//     console.log('no es primo')
+    
+// }
+
+
 
 module.exports = fizzBuzz;
+
